a90248b34e7b70e17adbabca807ba2c60d641d0b
JimZhang1105 <dy716581@dal.ca>
2024-08-30 22:36:32 -0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+namespace name {
+    double input;
+}
+void priceIsGood(double x);
+void enterNewPrice();
+int main() {
+    std::string input = " ";
+    std::cout<<"**********\n";
+
+    while(true) {
+        try{
+            std::cout<<"Enter price or type 'exit' to exit\n";
+            std::cin>>input;
+
+            if(input == "exit") {
+                std::cout<<"Exiting...\n";
+                break;
+            }
+
+            name::input = std::stod(input);
+            while(true) {
+                std::cout<<"Price entered: " <<name::input<< " Would you like to continue\n";
+                std::string option;
+
+                std::cout<<"y for Yes and n for N\n";
+
+                std::cin>>option;
+
+                if(option == "y" || option == "Y") {
+                    priceIsGood(name::input);
+                    break;
+
+                } else if(option == "n" || option == "N") {
+                    while(true) {
+                        std::cout<<"Any change you want to make?\n";
+                        std::string change;
+                        std::cout<<"y for Yes and n for N\n";
+
+                        std::cin>>change;
+                        if(change == "y" || change == "Y") {
+                            enterNewPrice();
+                            break;
+                        } else if(change == "n" || change == "N") {
+                            priceIsGood(name::input);
+                            break;
+                        } else {
+                            std::cout<<"Invalid input, enter y for Yes and n for N\n";
+                        }
+                    }
+                    break;
+                }
+            }
+        } catch(const std::invalid_argument&) {
+                std::cout<<"Error: Enter a price number or exit \n";
+        }
+
+    }
+    return 0;
+}
+
+void priceIsGood(double x) {
+    x = x * 1.15;
+    std::cout<<"Your price is: "<<x<<std::endl;
+}
+void enterNewPrice() {
+    while(true) {
+        try {
+            std::cout<<"Enter the new price\n";
+            double new_price;
+            std::cin>>new_price;
+
+                std::cout<<"New price is: " <<new_price<<"\n";
+                std::cout<<"Do you want to continue?\n";
+
+                std::string change2;
+                std::cin>>change2;
+                if(change2 == "y" || change2 == "Y") {
+                    priceIsGood(new_price);
+                    break;
+                }
+        } catch (const std::invalid_argument&) {
+            std::cout<<"Error: Enter a price number or exit \n";
+        }
+    }
+}
